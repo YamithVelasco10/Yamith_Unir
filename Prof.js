ae665aaec2cd68a2e025c3613d468a39f82e8767
YamithVelasco10 <106457655+YamithVelasco10@users.noreply.github.com>
2023-01-13 19:47:02 -0500

    Update Prof.js

--- Prof.js
+++ Prof.js
@@ -1,266 +1,266 @@
 [
    {
-      "partido":"PP",
-      "mediaAutoubicacion":7.09,
-      "votantes":2509,
+      "Alumno":"YAMITH VELASCO",
+      "Nota":10.09,
+      "Ranking":2509,
       "nsnc":302
    },
    {
-      "partido":"PSOE",
-      "mediaAutoubicacion":3.81,
-      "votantes":2330,
+      "Alumno":"PEDRO RODRÍGUEZ",
+      "Nota":3.81,
+      "Ranking":2330,
       "nsnc":260
    },
    {
-      "partido":"IU",
-      "mediaAutoubicacion":3,
-      "votantes":518,
+      "Alumno":"MARIA PEREZ",
+      "Nota":3,
+      "Ranking":518,
       "nsnc":14
    },
    {
-      "partido":"UPyD",
-      "mediaAutoubicacion":4.66,
-      "votantes":35,
+      "Alumno":"SANTIAGO GOYES",
+      "Nota":4.66,
+      "Ranking":35,
       "nsnc":1
    },
    {
-      "partido":"Ciudadanos",
-      "mediaAutoubicacion":5.25,
-      "votantes":1869,
+      "Alumno":"LENNIN FRANCO",
+      "Nota":5.25,
+      "Ranking":1869,
       "nsnc":160
    },
    {
-      "partido":"Podemos",
-      "mediaAutoubicacion":3.14,
-      "votantes":1751,
+      "Alumno":"CRISTIAN VILLA",
+      "Nota":3.14,
+      "Ranking":1751,
       "nsnc":117
    },
    {
-      "partido":"Convergència (CDC)",
-      "mediaAutoubicacion":4.96,
-      "votantes":192,
+      "Alumno":"ALEJANDRO MARINEZ",
+      "Nota":4.96,
+      "Ranking":192,
       "nsnc":16
    },
    {
-      "partido":"ERC",
-      "mediaAutoubicacion":2.92,
-      "votantes":282,
+      "Alumno":"CARLOS PERALTA",
+      "Nota":2.92,
+      "Ranking":282,
       "nsnc":8
    },
    {
-      "partido":"ICV",
-      "mediaAutoubicacion":3.33,
-      "votantes":57,
+      "Alumno":"DIANA CAICEDO",
+      "Nota":3.33,
+      "Ranking":57,
       "nsnc":1
    },
    {
-      "partido":"Unió",
-      "mediaAutoubicacion":4.94,
-      "votantes":12,
+      "Alumno":"PAOLA DIAZ",
+      "Nota":4.94,
+      "Ranking":12,
       "nsnc":1
    },
    {
-      "partido":"PNV",
-      "mediaAutoubicacion":4.96,
-      "votantes":113,
+      "Alumno":"ANDRES BENITEZ",
+      "Nota":4.96,
+      "Ranking":113,
       "nsnc":14
    },
    {
-      "partido":"EH Bildu",
-      "mediaAutoubicacion":2.49,
-      "votantes":125,
+      "Alumno":"LAURA ROJAS",
+      "Nota":2.49,
+      "Ranking":125,
       "nsnc":4
    },
    {
-      "partido":"Geroa Bai",
-      "mediaAutoubicacion":3.13,
-      "votantes":14,
+      "Alumno":"ESTEBAN ALOMÍA",
+      "Nota":3.13,
+      "Ranking":14,
       "nsnc":1
    },
    {
-      "partido":"UPN",
-      "mediaAutoubicacion":6.57,
-      "votantes":4,
+      "Alumno":"ESTEBAN ALOMOTO",
+      "Nota":6.57,
+      "Ranking":4,
       "nsnc":0
    },
    {
-      "partido":"BNG",
-      "mediaAutoubicacion":3.17,
-      "votantes":19,
+      "Alumno":"DRAKE BELL",
+      "Nota":3.17,
+      "Ranking":19,
       "nsnc":1
    },
    {
-      "partido":"Anova",
-      "mediaAutoubicacion":2.12,
-      "votantes":5,
+      "Alumno":"DROSS VAZQUEZ",
+      "Nota":2.12,
+      "Ranking":5,
       "nsnc":0
    },
    {
-      "partido":"CC",
-      "mediaAutoubicacion":5.38,
-      "votantes":14,
+      "Alumno":"CECILIA LEMO",
+      "Nota":5.38,
+      "Ranking":14,
       "nsnc":4
    },
    {
-      "partido":"Nueva Canarias",
-      "mediaAutoubicacion":4,
-      "votantes":4,
+      "Alumno":"CARMEN MORENO",
+      "Nota":4,
+      "Ranking":4,
       "nsnc":0
    },
    {
-      "partido":"Compromís",
-      "mediaAutoubicacion":3.05,
-      "votantes":52,
+      "Alumno":"LORENA AGUIRRE",
+      "Nota":3.05,
+      "Ranking":52,
       "nsnc":2
    },
    {
-      "partido":"FAC (Foro de Ciudadanos)",
-      "mediaAutoubicacion":4.5,
-      "votantes":2,
+      "Alumno":"TINO ROJAS",
+      "Nota":4.5,
+      "Ranking":2,
       "nsnc":0
    },
    {
-      "partido":"PAR (Partido Aragonés)",
-      "mediaAutoubicacion":5.81,
-      "votantes":4,
+      "Alumno":"PAR (JASON PIKS)",
+      "Nota":5.81,
+      "Ranking":4,
       "nsnc":0
    },
    {
-      "partido":"CHA",
-      "mediaAutoubicacion":3,
-      "votantes":1,
+      "Alumno":"JONAS CONTRERAS",
+      "Nota":3,
+      "Ranking":1,
       "nsnc":1
    },
    {
-      "partido":"PSM-Entesa/MES",
-      "mediaAutoubicacion":3.33,
-      "votantes":6,
+      "Alumno":"LUIS TAPIA",
+      "Nota":3.33,
+      "Ranking":6,
       "nsnc":0
    },
    {
-      "partido":"PI (Proposta per les Illes)",
-      "mediaAutoubicacion":4.5,
-      "votantes":2,
+      "Alumno":"GABRIELA TIPAN",
+      "Nota":4.5,
+      "Ranking":2,
       "nsnc":0
    },
    {
-      "partido":"VOX",
-      "mediaAutoubicacion":7.73,
-      "votantes":25,
+      "Alumno":"ESTEFANIA ARGUELLO",
+      "Nota":7.73,
+      "Ranking":25,
       "nsnc":0
    },
    {
-      "partido":"PACMA",
-      "mediaAutoubicacion":4,
-      "votantes":30,
+      "Alumno":"LAURA LEMOS",
+      "Nota":4,
+      "Ranking":30,
       "nsnc":11
    },
    {
-      "partido":"Los Verdes",
-      "mediaAutoubicacion":3.75,
-      "votantes":15,
+      "Alumno":"JAIME VARGAS",
+      "Nota":3.75,
+      "Ranking":15,
       "nsnc":8
    },
    {
-      "partido":"Partido Pirata",
-      "mediaAutoubicacion":3.13,
-      "votantes":1,
+      "Alumno":"ANTONIO VELASCO",
+      "Nota":3.13,
+      "Ranking":1,
       "nsnc":0
    },
    {
-      "partido":"Escaños en Blanco",
-      "mediaAutoubicacion":3.6,
-      "votantes":4,
+      "Alumno":"MIRIAM VELASCO",
+      "Nota":3.6,
+      "Ranking":4,
       "nsnc":1
    },
    {
-      "partido":"España 2000",
-      "mediaAutoubicacion":5,
-      "votantes":2,
+      "Alumno":"ARTURO VELASCO",
+      "Nota":5,
+      "Ranking":2,
       "nsnc":0
    },
    {
-      "partido":"Ahora en Común",
-      "mediaAutoubicacion":2,
-      "votantes":6,
+      "Alumno":"CARMEN LEON",
+      "Nota":2,
+      "Ranking":6,
       "nsnc":0
    },
    {
-      "partido":"(En Cataluña) En Comú Podem/Podem en Comú",
-      "mediaAutoubicacion":5,
-      "votantes":0,
+      "Alumno":"MARCELO REYES",
+      "Nota":5,
+      "Ranking":0,
       "nsnc":0
    },
    {
-      "partido":"Unidad Popular",
-      "mediaAutoubicacion":3.48,
-      "votantes":2,
+      "Alumno":"SANDRITA REYES",
+      "Nota":3.48,
+      "Ranking":2,
       "nsnc":0
    },
    {
-      "partido":"(En Galicia) En Marea",
-      "mediaAutoubicacion":2.49,
-      "votantes":4,
+      "Alumno":"KEVIN PEREZ",
+      "Nota":2.49,
+      "Ranking":4,
       "nsnc":0
    },
    {
-      "partido":"Partido Cannabis",
-      "mediaAutoubicacion":1.81,
-      "votantes":3,
+      "Alumno":"SONIA REYES",
+      "Nota":1.81,
+      "Ranking":3,
       "nsnc":2
    },
    {
-      "partido":"Falange Española",
-      "mediaAutoubicacion":9.04,
-      "votantes":3,
+      "Alumno":"JUNO PEREZ",
+      "Nota":9.04,
+      "Ranking":3,
       "nsnc":0
    },
    {
-      "partido":"Democracia Nacional",
-      "mediaAutoubicacion":10,
-      "votantes":0,
+      "Alumno":"NICO REYES",
+      "Nota":10,
+      "Ranking":0,
       "nsnc":0
    },
    {
-      "partido":"SAIn (Partido Solidaridad y Autogestión Internacionalista)",
-      "mediaAutoubicacion":5,
-      "votantes":1,
+      "Alumno":"DANIEL RAMOS",
+      "Nota":5,
+      "Ranking":1,
       "nsnc":0
    },
    {
-      "partido":"Otro",
-      "mediaAutoubicacion":3.07,
-      "votantes":108,
+      "Alumno":"LEONARDO CAMPOS",
+      "Nota":3.07,
+      "Ranking":108,
       "nsnc":5
    },
    {
-      "partido":"Voto nulo",
-      "mediaAutoubicacion":4.3,
-      "votantes":87,
+      "Alumno":"GALO ANDRADE",
+      "Nota":4.3,
+      "Ranking":87,
       "nsnc":20
    },
    {
-      "partido":"En blanco",
-      "mediaAutoubicacion":4.66,
-      "votantes":373,
+      "Alumno":"WILLIAM ANDRADE",
+      "Nota":4.66,
+      "Ranking":373,
       "nsnc":116
    },
    {
-      "partido":"No votaría",
-      "mediaAutoubicacion":4.62,
-      "votantes":760,
+      "Alumno":"TINO PEREZ",
+      "Nota":4.62,
+      "Ranking":760,
       "nsnc":638
    },
    {
-      "partido":"No sabe todavía",
-      "mediaAutoubicacion":4.59,
-      "votantes":2541,
+      "Alumno":"JOSUE RAMOS",
+      "Nota":4.59,
+      "Ranking":2541,
       "nsnc":786
    },
    {
-      "partido":"N.C.",
-      "mediaAutoubicacion":4.75,
-      "votantes":733,
+      "Alumno":"ALEX RAMOS",
+      "Nota":4.75,
+      "Ranking":733,
       "nsnc":340
    }
 ]
